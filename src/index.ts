66e5ce424950f79cf2741ec474fe45b693c37a01
Shadow <william.shadow@outlook.com>
2022-07-09 07:38:04 +0000

    ugh

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { Client, GuildMember, PresenceStatus } from "discord.js"
+import dotEnvExtended from "dotenv-extended"
+dotEnvExtended.load()
+
+import { REST } from "@discordjs/rest"
+import {
+    APIGuildMember,
+    APIGuildWidget,
+    APIUser,
+    GuildWidgetStyle,
+    Routes,
+} from "discord-api-types/v10"
 import express, { Application } from "express"
+import * as deepAi from "deepai"
+
+deepAi.default.setToken(process.env.DEEPAI)
+
+const management = "958566416691367937"
 
 const app: Application = express()
-const client = new Client({ intents: "GUILD_MEMBERS" })
-client.login(process.env.TOKEN)
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN)
+
+console.log(`Started`)
 
 app.get("/", (req, res) => {
     res.sendStatus(200)
@@ -11,21 +27,30 @@ app.get("/", (req, res) => {
 
 app.get("/status/:id", async (req, res) => {
     const { id } = req.params
-    let member: GuildMember
-    client.guilds.cache.every((guild) => {
-        const mem = guild.members.resolve(id)
-        if(mem) {
-            member = mem
-            return false
-        }
-    })
 
-    if(!member || !member.user.bot) return res.sendStatus(404)
+    const user = (await rest.get(Routes.user(id))) as APIUser
+    if (!user) return res.sendStatus(404)
 
-    const status: PresenceStatus = member.presence.status
+    const widget = (await rest.get(
+        Routes.guildWidgetJSON(management)
+    )) as APIGuildWidget
+    if (!widget) return res.sendStatus(500)
 
-    if(status === "offline") return res.sendStatus(503)
-    return res.sendStatus(200)
+    console.log(widget, user)
 
+    const found = widget.members.filter((x) => x.username === user.username)
+    let result
+    await Promise.all(
+        result = found.map(async (x) => {
+            return await deepAi.default.callStandardApi("image-similarity", {
+                image1: x.avatar_url,
+                image2: `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`,
+            })
+        })
+    )
+    console.log(found, result)
+    if (found) return res.json(found)
+
+    return res.sendStatus(503)
 })
-app.listen(process.env.PORT)
+app.listen(8080)
